7711ed71d89624d4f1645a89dc6d203457bb3b15
dfc <danielfrancocecilia@gmail.com>
2016-07-13 12:37:36 +0200

    tennis-kata completed

--- tennis-kata/src/TennisGameImpl.ts
+++ tennis-kata/src/TennisGameImpl.ts
@@ -1,12 +1,75 @@
 /// <reference path="../references.ts" />
 
 class TennisGameImpl implements TennisGame {
+    scoreA:number = 0;
+    scoreB:number = 0;
+    pointsA:number = 0;
+    pointsB:number = 0;
+    incrementUntil2Games:number = 15;
+    incrementFrom2Games:number = 10;
 
-  wonPoint(player: Player) {
+    wonPoint(player:Player) {
+        if (Player.A == player) {
+            if (this.scoreA < 30)
+                this.scoreA += this.incrementUntil2Games;
+            else if (this.scoreA == 30)
+                this.scoreA += this.incrementFrom2Games;
+            else
+                this.pointsA += 1;
+        } else if (Player.B == player) {
+            if (this.scoreB < 30)
+                this.scoreB += this.incrementUntil2Games;
+            else if (this.scoreB == 30)
+                this.scoreB += this.incrementFrom2Games;
+            else
+                this.pointsB += 1;
+        }
+    }
 
-  }
+    getScore():string {
+        if (this.scoreA == this.scoreB)
+            return this.getScoreWhenScoresAreEq();
+        else if (this.scoreA > this.scoreB)
+            return this.getScoreWhenPlayerAScoreIsGreaterThanB();
+        else
+            return this.getScoreWhenPlayerBScoreIsGreaterThanA();
+    }
 
-  getScore(): string {
-    return "I don't know?";
-  }
+    private getScoreWhenScoresAreEq():string {
+        if (this.scoreA == 0)
+            return "love-all";
+        else if (this.scoreA < 40)
+            return this.scoreA + "-all";
+        else if (this.scoreA == 40)
+            return this.getScoreWhenScoresAreEqTo40();
+    }
+
+    private getScoreWhenScoresAreEqTo40():string {
+        if (this.pointsA == this.pointsB)
+            return "deuce";
+        else if (this.pointsA > this.pointsB)
+            if (this.pointsA - this.pointsB < 2)
+                return "Advantage A";
+            else
+                return "Game A";
+        else if (this.pointsA < this.pointsB)
+            if (this.pointsB - this.pointsA < 2)
+                return "Advantage B";
+            else
+                return "Game B";
+    }
+
+    private getScoreWhenPlayerAScoreIsGreaterThanB():string {
+        if (this.scoreA - this.scoreB == this.scoreA)
+            return this.scoreA + "-love";
+        else if (this.pointsA != 0)
+            return "Game A";
+    }
+
+    private getScoreWhenPlayerBScoreIsGreaterThanA():string {
+        if (this.scoreB - this.scoreA == this.scoreB)
+            return "love-" + this.scoreB;
+        else if (this.pointsB != 0)
+            return "Game B";
+    }
 }
--- tennis-kata/test/TennisGameSpec.ts
+++ tennis-kata/test/TennisGameSpec.ts
@@ -1,17 +1,142 @@
 /// <reference path='../typings/jasmine/jasmine.d.ts' />
 /// <reference path='../references.ts' />
 
-describe('For a TennisGame', function() {
+describe('For a TennisGame', function () {
+
+    var g:TennisGame;
+
+    beforeEach(function () {
+        g = new TennisGameImpl();
+    });
+
+    it('when it has just started, the score should be "love-all"', function () {
+        expect(g.getScore()).toBe('love-all');
+    });
+
+    it('when A player wins a game and B player none, the score should be "15-love"', function() {
+        g.wonPoint(Player.A);
+        expect(g.getScore()).toBe('15-love');
+    });
+
+    it('when A player wins 2 games and B player none, the score should be "30-love"', function() {
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        expect(g.getScore()).toBe('30-love');
+    });
+
+    it('when A player wins 3 games and B player none, the score should be "40-love"', function() {
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        expect(g.getScore()).toBe('40-love');
+    });
+
+    it('when A player wins 1 game and B player wins 1 game, the score should be "15-all"', function() {
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.B);
+        expect(g.getScore()).toBe('15-all');
+    });
+
+    it('when A player wins 2 games and B player wins 2 games, the score should be "30-all"', function() {
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        expect(g.getScore()).toBe('30-all');
+    });
+
+    it('when A player wins 3 games and B player wins 3 games, the score should be "deuce"', function() {
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        expect(g.getScore()).toBe('deuce');
+    });
+
+    it('when A player wins 4 games and B player wins 3 games, the score should be "Advantage A"', function() {
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        expect(g.getScore()).toBe('Advantage A');
+    });
+
+    it('when A player wins 4 games and B player wins 2 games, the score should be "Game A"', function() {
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        expect(g.getScore()).toBe('Game A');
+    });
+
+    it('when A player wins 5 games and B player wins 3 games, the score should be "Game A"', function() {
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        expect(g.getScore()).toBe('Game A');
+    });
     
-  var g: TennisGame;
+    it('when B player wins a game and A player none, the score should be "love-15"', function() {
+        g.wonPoint(Player.B);
+        expect(g.getScore()).toBe('love-15');
+    });
 
-  beforeEach(function() {
-    g = new TennisGameImpl();
-  });
+    it('when B player wins 2 game and A player none, the score should be "love-30"', function() {
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        expect(g.getScore()).toBe('love-30');
+    });
 
-  it('when it has just started, the score should be "love-all"', function() {
-    expect(g.getScore()).toBe('love-all');
-  });
-  
+    it('when B player wins 3 game and A player none, the score should be "love-40"', function() {
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        expect(g.getScore()).toBe('love-40');
+    });
+
+    it('when B player wins 4 games and A player wins 3 games, the score should be "Advantage B"', function() {
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        expect(g.getScore()).toBe('Advantage B');
+    });
+
+    it('when B player wins 4 games and A player wins 2 games, the score should be "Game B"', function() {
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        expect(g.getScore()).toBe('Game B');
+    });
+
+    it('when B player wins 5 games and A player wins 3 games, the score should be "Game B"', function() {
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.A);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        g.wonPoint(Player.B);
+        expect(g.getScore()).toBe('Game B');
+    });
 });
 
